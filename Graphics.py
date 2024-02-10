89b9dfc1aafe2f9cc68e209d0c2ad692a5d26a16
rrayane667 <122290392+rrayane667@users.noreply.github.com>
2024-02-10 03:22:09 +0100

    Update Graphics.py

--- Graphics.py
+++ Graphics.py
@@ -1,67 +1,78 @@
-from math import sin, cos, sqrt
+from math import sin, cos, sqrt, floor
 class graphics_pipeline :
-    def __init__(self, vertex_position, vertex_color, ordre, proj, dim, draw, t, res, a, b, c):
-        self.vertex_position = vertex_position
+    def __init__(self, vertex_color, proj, dimx, dimy, draw, t, res, center, disc, grad):
+        self.vertex_position = []
         self.vertex_color = vertex_color
-        self.ordre = ordre
         self.proj = proj
-        self.dim = dim
+        self.dimx = dimx
+        self.dimy = dimy
         self.draw = draw
         self.t = t
         self.res = res
-        self.a = a
-        self.b = b
-        self.c = c
-    def CamRot(self, v, sx, sy):
-        x = v[0]
-        y = v[1]
-        z = v[2]
-        theta = sx
-        phi = sy
-        zeta = 0
-        Fx=(x*cos(theta)-z*sin(theta))*cos(zeta)-(y*cos(phi)-x*sin(theta)*sin(phi)-z*cos(theta)*sin(phi))*sin(zeta)
-        Fy=(x*cos(theta)-z*sin(theta))*sin(zeta)+cos(zeta)*(y*cos(phi)-x*sin(theta)*sin(phi)-z*cos(theta)*sin(phi))
-        Fz=y*sin(phi)+x*sin(theta)*cos(phi)+z*cos(phi)*cos(theta)
-        return [Fx, Fy, Fz]
-    def tesselation_shader(self, res, t):
-        dim = self.dim
+        self.center = center
+        self.disc = disc
+        self.triangles = []
+        self.normales = []
+        self.grad = grad
+
+    #subdivide plane and disceplace vertices
+    def vertexShader(self):
+        dimx = self.dimx
+        dimy = self.dimy
+        res = self.res
         coord=[[] for _ in range(res)]
         for i in range(res):
             for j in range(res):
-                x=i*dim/res - (1-i)*dim/res
-                y=j*dim/res - (1-j)*dim/res
-                coord[i].append((x, y, 20*sin(self.a*x + self.b*y + self.c*t)))
-        return coord
+                x=i*dimx/res - (res-i)*dimx/res
+                y=j*dimy/res - (res-j)*dimy/res
+                coord[i].append(self.disc(x, y))
+        self.vertex_position = coord
     
-    def normals(self, res, dim, t):
-        normals = [[] for _ in range(res)]
+    #calculate normals
+    def normals(self):
+        dimx = self.dimx
+        dimy = self.dimy
+        res = self.res
         for i in range(res):
             for j in range(res):
-                x=i*dim/res - (1-i)*dim/res
-                y=j*dim/res - (1-j)*dim/res
-                l=sqrt((self.a**2 + self.b**2)*cos(self.a*x + self.b*y + self.c*t)**2 + 1)
-                normals[i].append((-self.b*cos(self.a*x + self.b*y + self.c*t)/l, self.a*cos(self.a*x + self.b*y + self.c*t)/l, 1/l))
-        return normals
-    def scalaire(self, v, u): return u[0]*v[0] + u[1]*v[1] + u[2]*v[2]
-        
-    def triangularisation(self, c, clr, t):
-        n=len(c[0])
-        f=self.draw
-        norm = self.normals(self.res, self.dim, t)
-        if not clr: clr=[[1 for _ in c[i]] for i in range(len(c))]
-        for i in range(n-1):
-            for j in range(n-1):
-                l=sqrt((400-c[i][j][0])**2 + (400-c[i][j][1])**2 + (400-c[i][j][2])**2)
-                lum = max(0, self.scalaire(norm[i][j], [(400-c[i][j][0])/l, (400-c[i][j][1])/l, (400-c[i][j][2])/l]))
-                f([[self.proj(c[i][j]), self.proj(c[i][j+1]), self.proj(c[i+1][j+1])], (255*lum, 255*lum, 255*lum)])
-                f([[self.proj(c[i][j]), self.proj(c[i+1][j]), self.proj(c[i+1][j+1])], (255*lum, 255*lum, 255*lum)])
-                
-                #print([self.proj(c[i][j]), self.proj(c[i][j+1]), self.proj(c[i+1][j+1])])
-    
-    def rendering(self, sx, sy, t):
-        new_coord = self.tesselation_shader(self.res, t) #[ [self.proj(self.tesselation_shader(self.res)[i][j]) for j in range(len(self.tesselation_shader(self.res)[i]))] for i in range(len(self.tesselation_shader(self.res)))]
-        rot_coord = [ [self.CamRot(j, sx, sy) for j in new_coord[i]] for i in range(len(new_coord))]
-        self.triangularisation(rot_coord, self.vertex_color, t)
+                x=i*dimx/res - (res-i)*dimx/res
+                y=j*dimy/res - (res-j)*dimy/res
+                g = self.grad(x, y)
+                B = [0, 1, g[2]]
+                T = [1, 0, g[0]]
+                self.normales.append(Normalize(cross(T, B)))
 
-        
-        
+    #color
+    def fragShader(self):
+        self.vertex_color = [[[120, 0, 255] for _ in range(len(self.vertex_position[0]))] for _ in range(len(self.vertex_position))]
+
+    #generate triangles
+    def triangularisation(self):
+        c = self.vertex_position
+        self.triangles = [[self.proj(c[i][j]), self.proj(c[i][j+1]), self.proj(c[i+1][j+1])] for j in range(len(c[0])-1) for i in range(len(c[0])-1)]
+        self.triangles.extend([[self.proj(c[i+1][j+1]), self.proj(c[i][j]), self.proj(c[i+1][j])] for j in range(len(c[0])-1) for i in range(len(c[0])-1)])
+
+    #combine function to render
+    def rendering(self):
+        self.normals()
+        self.vertexShader()
+        self.fragShader()
+        self.triangularisation()
+
+def scalaire(v, u): return u[0]*v[0] + u[1]*v[1] + u[2]*v[2]
+def cross(a, b):
+    return [a[1]*b[2] - a[2]*b[1],
+            a[2]*b[0] - a[0]*b[2],
+            a[0]*b[1] - a[1]*b[0]]
+
+
+def diffVect(u, v):
+    return [u[0]-v[0], u[1]-v[1], u[2]-v[2]]
+def addVect(u, v):
+    return [u[0]+v[0], u[1]+v[1], u[2]+v[2]]
+
+
+def Normalize(u):
+    r = sqrt(u[0]**2 + u[1]**2 + u[2]**2)
+    if not r: return u
+    return [u[0]/r, u[1]/r, u[2]/r]
